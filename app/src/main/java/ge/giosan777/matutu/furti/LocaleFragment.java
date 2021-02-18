3d9b41d9e9178d1fc3bda2a30d7c05f5185dcc70
giosan777 <giosan777@gmail.com>
2021-02-19 00:06:22 +0400

    lokalis asarchevi gverdi

---
+++ app/src/main/java/ge/giosan777/matutu/furti/LocaleFragment.java
@@ -0,0 +1,19 @@
+package ge.giosan777.matutu.furti;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class LocaleFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_locale, container, false);
+    }
+}
--- app/src/main/java/ge/giosan777/matutu/furti/MainActivity.java
+++ app/src/main/java/ge/giosan777/matutu/furti/MainActivity.java
@@ -1,14 +1,22 @@
 package ge.giosan777.matutu.furti;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Fragment localeFragment = new LocaleFragment();
+    FragmentManager fragmentManager = getSupportFragmentManager();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragmentManager.beginTransaction().add(R.id.fragment_container, localeFragment).commit();
+
     }
 }
